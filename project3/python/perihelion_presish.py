88b75f560e1db19648c595caf4e349c9a5b12997
Gunnar <gunnartl@uio.no>
2018-10-21 20:25:46 +0200

    blah

--- project3/python/perihelion_presish.py
+++ project3/python/perihelion_presish.py
@@ -14,7 +14,7 @@ mercury  = rc.body(mass_mercury,p0_mercury,v0_mercury)
 sun      = rc.body(mass_sun,p0_sun,v0_sun)
 
 system = rc.system(sun,mercury)
-N = int(1e6)
+N = int(1.5e6)
 forsøk = rc.solve(system,N,1e-6)
 forsøk.verlet()
 
@@ -36,11 +36,18 @@ for i in range(1,N-1):
     r_prev = np.linalg.norm(system[1].pos_vec[i-1,:]-system[0].pos_vec[i-1,:])
     if r_now < r_next and r_now < r_prev:
         periheels.append(system[1].pos_vec[i,:])
-        
-x0 = periheels[0][0]
-y0 = periheels[0][1]
+#%% 
+periheels = np.array(periheels)
+x0 = periheels[1][0]
+y0 = periheels[1][1]
 
-xf = periheels[-1][0]
-yf = periheels[-1][1]
+xf = periheels[-2][0]
+yf = periheels[-2][1]
 
-print ((np.arctan(y0/x0)-np.arctan(yf/xf)) * (180*3600/np.pi))
+
+print(periheels)
+plt.plot(tans)
+plt.show()
+
+print(len(periheels))
+print ((np.arctan(y0/x0)-np.arctan(yf/xf)) *180/np.pi)
